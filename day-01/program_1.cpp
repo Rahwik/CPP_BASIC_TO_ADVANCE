b44cfa5f70f104353985bf86703296f57fb4c52e
RAHUL PRASAD <157337927+Rahwik@users.noreply.github.com>
2024-08-05 14:15:53 +0530

    Update program_1.cpp

--- day-01/program_1.cpp
+++ day-01/program_1.cpp
@@ -1,6 +1,6 @@
 //print hello world using c++
-#include<iostream>
-int main()
+#include<iostream> //package or library
+int main() //Entry point fom where the main complilation starts
 {
 std::cout<<"hello world";
 return 0;
